ebe0c7aa68626ab247b15546664024794fa63704
Ben Schwabe <bspymaster@gmail.com>
2024-11-05 22:25:54 -0600

    added variable enemy spawning and selector

--- scripts/manager.gd
+++ scripts/manager.gd
@@ -6,15 +6,18 @@ var enemy_spawn_range: Vector2i
 @export
 var enemy_scene_path: String
 
+var selected_target: int = 0
 var targetables: Array[Enemy] = []
 
 
 func _ready():
 	_spawn_baddies()
+	_select_targetable(selected_target)
 
 
 func _spawn_baddies():
-	for enemy_count in range(0, randi_range(enemy_spawn_range.x, enemy_spawn_range.y)):
+	var x_spawn = 64
+	for enemy_counter in range(0, randi_range(enemy_spawn_range.x, enemy_spawn_range.y)):
 		var enemy = Enemy.new()
 		enemy.health_points = 1
 		enemy.attack_points = 1
@@ -22,5 +25,19 @@ func _spawn_baddies():
 		
 		var enemy_scene = load(enemy_scene_path)
 		var enemy_node: Node2D = enemy_scene.instantiate()
-		enemy_node.position = Vector2(128,64)
+		enemy_node.position = Vector2(x_spawn,64)
+		enemy.enemy_scene = enemy_node
 		add_child(enemy_node)
+		x_spawn += 32
+
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("change target"):
+		selected_target += 1
+		_select_targetable(selected_target % targetables.size())
+
+
+func _select_targetable(index: int):
+	if targetables.size() <= index:
+		return
+	$Pointer.position = targetables[index].enemy_scene.position - Vector2(0, 16)
