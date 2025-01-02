e50308265ea6a53d7f8e2314f0b35174297c7864
Ben Schwabe <bspymaster@gmail.com>
2025-01-02 12:37:14 -0600

    Added a separate grid state
    Created a class to store the grid state separately from the grid template

---
+++ resources/grid/levelGrid.gd
@@ -0,0 +1,41 @@
+class_name LevelGrid
+
+
+## The grid coordinates of the room the player is currently in
+var current_room_coords = Vector2i()
+
+var _room_grid: Array[Array]
+
+
+func new():
+	reset_grid()
+
+
+## Adds a room to the room grid, leaving empty spaces in the grid along the way if needed
+func add_room(room_coords: Vector2i, room_data: EnemyRoomOption):
+	# Init the grid if needed
+	if _room_grid == null:
+		_room_grid = []
+	# expand the grid rows if needed
+	while _room_grid.size() <= room_coords.x:
+		_room_grid.append([])
+	# expand the grid columns if needed
+	while _room_grid[room_coords.x].size() < room_coords.y:
+		_room_grid[room_coords.x].append(null)
+	_room_grid[room_coords.x].append(room_data)
+
+
+## Retrieves the room at the given coordinates
+func get_room(room_coords: Vector2i) -> EnemyRoomOption:
+	return _room_grid[room_coords.x][room_coords.y]
+
+
+## Resets the grid to an empty state, preserving the created grid dimensions if needed
+func reset_grid(preserve_dimensions: bool = false):
+	if !preserve_dimensions:
+		_room_grid = []
+		return
+	
+	for room_row in _room_grid:
+		for y in range(0,room_row.size()):
+			room_row[y] = null
--- resources/grid/levelGridTemplate.gd
+++ resources/grid/levelGridTemplate.gd
@@ -7,20 +7,15 @@ var grid_dimensions: Vector2i
 @export
 var room_options: Array[EnemyRoomOption]
 
-var current_room_coords = Vector2i()
-var _room_grid: Array[Array] = []
 
-
-func generate_grid():
-	for i in range(0, grid_dimensions.x):
-		_room_grid.append([])
-		for j in range(0, grid_dimensions.y):
+## Generates a grid instance based on its configuration
+func generate_grid() -> LevelGrid:
+	var level_grid = LevelGrid.new()
+	for x in range(0, grid_dimensions.x):
+		for y in range(0, grid_dimensions.y):
 			var selected_room_option = room_options.pick_random()
 			
-			# TODO: need a new way to store GENERATED rooms versus their potential options
-			#       This will just reuse the same class instance for every room it's used and keep overwriting itself
 			selected_room_option.generate_targetables()
-			_room_grid[i].append(selected_room_option)
-
-func get_room(room_coords: Vector2i) -> EnemyRoomOption:
-	return _room_grid[room_coords.x][room_coords.y]
+			level_grid.add_room(Vector2(x,y), selected_room_option)
+			
+	return level_grid
